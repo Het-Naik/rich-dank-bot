79a2ca4326ce2c39c645867a893a29cf0b6d87dc
Het-Naik <76436467+Het-Naik@users.noreply.github.com>
2020-12-23 11:58:31 +0530

    Update dis spam bot.py

--- RICH BOT/dis spam bot.py
+++ RICH BOT/dis spam bot.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 test_driver = webdriver.Chrome('/Users/mukti/chromedriver')
-test_driver.get('https://discord.com/channels/763021076283260958/775588110011596800')
+test_driver.get('linkOfTheTextChannelWhereYouWantToSpam')
 
 time.sleep(3)
 
@@ -12,7 +12,7 @@ gmail = test_driver.find_element_by_xpath('/html/body/div/div[2]/div/div[2]/div/
 gmail.send_keys(email)
 
 
-psw = 'yourdiscordPassword'
+psw = 'yourDiscordPassword'
 passs = test_driver.find_element_by_xpath('/html/body/div/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/div[2]/div/input')
 passs.send_keys(psw)
 
@@ -21,18 +21,12 @@ login = test_driver.find_element_by_xpath('/html/body/div/div[2]/div/div[2]/div/
 login.click()
 
 time.sleep(30)
-
-for i in range(0,999999):
+i = 0
+while i == 0:
         message = 'pls beg'
-        message2 = 'pls dep max'
         text_box = test_driver.find_element_by_css_selector('#app-mount > div.app-1q1i1E > div > div.layers-3iHuyZ.layers-3q14ss > div > div > div > div > div.chat-3bRxxu > div > main > form > div > div > div > div > div.textArea-12jD-V.textAreaSlate-1ZzRVj.slateContainer-3Qkn2x > div.markup-2BOw-j.slateTextArea-1Mkdgw.fontSize16Padding-3Wk7zP > div')
         text_box.send_keys(message)
         test_driver.find_element_by_css_selector("#app-mount > div.app-1q1i1E > div > div.layers-3iHuyZ.layers-3q14ss > div > div > div > div > div.chat-3bRxxu > div > main > form > div > div > div > div > div.textArea-12jD-V.textAreaSlate-1ZzRVj.slateContainer-3Qkn2x > div.markup-2BOw-j.slateTextArea-1Mkdgw.fontSize16Padding-3Wk7zP > div").send_keys(Keys.RETURN)
-        time.sleep(1)
-        text_box = test_driver.find_element_by_css_selector('#app-mount > div.app-1q1i1E > div > div.layers-3iHuyZ.layers-3q14ss > div > div > div > div > div.chat-3bRxxu > div > main > form > div > div > div > div > div.textArea-12jD-V.textAreaSlate-1ZzRVj.slateContainer-3Qkn2x > div.markup-2BOw-j.slateTextArea-1Mkdgw.fontSize16Padding-3Wk7zP > div')
-        text_box.send_keys(message2)
-        test_driver.find_element_by_css_selector("#app-mount > div.app-1q1i1E > div > div.layers-3iHuyZ.layers-3q14ss > div > div > div > div > div.chat-3bRxxu > div > main > form > div > div > div > div > div.textArea-12jD-V.textAreaSlate-1ZzRVj.slateContainer-3Qkn2x > div.markup-2BOw-j.slateTextArea-1Mkdgw.fontSize16Padding-3Wk7zP > div").send_keys(Keys.RETURN)
         time.sleep(46)
-        
 
 
